d109953f98bc3221bb2773c393aa81394de83848
Unknown <karocksjoelee@gmail.com>
2018-08-29 16:19:30 +0800

    [ Node ] Render Angular App at default route

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public')); // Server Side Render Engine
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist/mean-ts-starter'))); // Point to Angular App's folder
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -35,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
